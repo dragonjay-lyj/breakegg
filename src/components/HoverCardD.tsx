d63f41281e70cb596f92d9dc828e916fa5139f10
dragonjay-lyj <liujinlisb@gmail.com>
2024-01-31 21:18:14 +0800

    deleted:    src/components/CodeBlock.astro 	modified:   src/components/HoverCardD.tsx

--- src/components/HoverCardD.tsx
+++ src/components/HoverCardD.tsx
@@ -23,8 +23,8 @@ const HoverCardD = ({ tsrc, username, tdes, thref, tpubDate, pixiv }) => {
     <HoverCard>
   <HoverCardTrigger asChild>
     <div className="grid grid-cols-2" data-aos="fade-up">
-      <div className="aspect-square h-full w-full overflow-hidden rounded-2xl bg-blue-50 shadow-md duration-300 hover:scale-[1.05] md:h-56 md:shadow-none">
-        <div className="dark:bg-[#111827] flex h-2/3 flex-row justify-center gap-4 bg-white md:h-3/5 md:justify-evenly">
+      <div className="aspect-square h-full w-full overflow-hidden rounded-2xl bg-blue-50 shadow-md duration-300 hover:scale-[1.05] hover:shadow-lg md:h-56 md:shadow-none">
+        <div className="dark:bg-gray-800 flex h-2/3 flex-row justify-center gap-4 bg-white md:h-3/5 md:justify-evenly">
           <div className="flex w-20 items-center justify-center md:w-1/5">
             <Avatar>
               <AvatarImage src={tsrc} />
@@ -32,11 +32,11 @@ const HoverCardD = ({ tsrc, username, tdes, thref, tpubDate, pixiv }) => {
             </Avatar>
           </div>
           <div className="flex w-2/5 flex-col items-center justify-center md:items-start">
-          <h4 className="text-sm font-semibold text-black dark:text-white">{username}</h4>
+            <h4 className="text-sm font-semibold text-black dark:text-white">{username}</h4>
             <div className="hidden md:block md:text-4xl"></div>
           </div>
         </div>
-        <div className="dark:bg-[#111827] flex h-1/3 flex-row items-center justify-evenly bg-gray-50 md:h-2/5">
+        <div className="dark:bg-gray-800 flex h-1/3 flex-row items-center justify-evenly bg-gray-50 md:h-2/5">
           <Button className="w-full sm:w-auto inline-flex items-center justify-center px-4 py-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none" href={thref} as={Link}><FontAwesomeIcon icon={faXTwitter} /> Twitter</Button>
           <Button
             className="w-full sm:w-auto inline-flex items-center justify-center px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none"
